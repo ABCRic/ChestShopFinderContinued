e1dc9e2c722410f0e53cf9c0b2cea87a83c1b90d
Cyprias <Cyprias@gmail.com>
2013-05-13 05:05:20 -0600

    * Our verifyChunk shop code runs the DB call asyncly then does the in game stuff in the main thread. * During the verify chunk check, we check stock amount and update our DB.

--- src/com/cyprias/ChestShopFinder/listeners/WorldListener.java
+++ src/com/cyprias/ChestShopFinder/listeners/WorldListener.java
@@ -7,12 +7,19 @@ import java.util.List;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.Acrobot.Breeze.Utils.InventoryUtil;
+import com.Acrobot.Breeze.Utils.MaterialUtil;
+import com.Acrobot.ChestShop.Signs.ChestShopSign;
+import com.Acrobot.ChestShop.Utils.uBlock;
 import com.cyprias.ChestShopFinder.Logger;
 import com.cyprias.ChestShopFinder.Plugin;
 import com.cyprias.ChestShopFinder.configuration.Config;
@@ -24,7 +31,7 @@ public class WorldListener implements Listener {
 		ChunkLoadEvent.getHandlerList().unregister(instance);
 	}
 
-	public static HashMap<Chunk, Boolean> checkedChunks = new HashMap<Chunk, Boolean>();
+	public static HashMap<String, Boolean> checkedChunks = new HashMap<String, Boolean>();
 	
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onChunkLoad(ChunkLoadEvent event) throws SQLException {
@@ -34,10 +41,10 @@ public class WorldListener implements Listener {
 
 		final Chunk chunk = event.getChunk();
 		
-		if (checkedChunks.containsKey(chunk))
+		String t = event.getChunk().getX() + " " + event.getChunk().getZ();
+		if (checkedChunks.containsKey(t))
 			return;
-		
-		checkedChunks.put(chunk, true);
+		checkedChunks.put(t, true);
 		
 		/*
 		for (BlockState state : chunk.getTileEntities()) {
@@ -65,24 +72,64 @@ public class WorldListener implements Listener {
 			
 	//	Logger.debug("onChunkLoad "+chunk.getX() + " " + chunk.getZ() +" | " + aBlock.getLocation().getBlockX() + " to " + bBlock.getLocation().getBlockX());
 		
+		// Run our DB call in a async thread.
 		Plugin.getInstance().getServer().getScheduler().runTaskAsynchronously(Plugin.getInstance(), new Runnable() {
 			public void run() {
-				
-				
-				
+
 				try {
-					List<Shop> shops = Plugin.database.getShopsInCoords(chunk.getWorld().getName(), xStart, xEnd, zStart, zEnd);
+				//	Logger.debug("Checking for missing shops between X: " + xStart + " - " + xEnd + ", Z: " + zStart + " - " + zEnd);
 					
-					for (int i=0; i<shops.size(); i++){
-						
-						if (!shopExists(shops.get(i))){
-							Logger.warning("Shop #" + shops.get(i).id + " no longer exists, removing from DB.");
-							shops.get(i).delete();
-						}
-					}
+					final List<Shop> shops = Plugin.database.getShopsInCoords(chunk.getWorld().getName(), xStart, xEnd, zStart, zEnd);
 					
 					
-					
+					// Run our check in the main thread. 
+					Plugin.getInstance().getServer().getScheduler().runTask(Plugin.getInstance(), new Runnable() {
+						public void run() {
+							Sign sign;
+							for (int i=0; i<shops.size(); i++){
+								
+								sign = shopExists(shops.get(i));
+								
+								if (sign == null){
+									Logger.warning("Shop #" + shops.get(i).id + " no longer exists, removing from DB.");
+									try {
+										Plugin.database.deleteShop(shops.get(i).id);
+									} catch (SQLException e) {
+										Logger.warning("Exception caught while lookign up shops in chunk.");
+										e.printStackTrace();
+									}
+									
+								}else{
+									
+									int inStock = 0;
+
+									String owner = sign.getLines()[0];
+									ItemStack stock = MaterialUtil.getItem(sign.getLines()[3]);
+
+									if (ChestShopSign.isAdminShop(owner)) {
+										inStock = 64 * 9 * 6; // Full chest. :P
+									} else {
+										Chest chest = uBlock.findConnectedChest(sign.getBlock());
+										if (chest != null) {
+											inStock = InventoryUtil.getAmount(stock, chest.getInventory());
+										}
+									}
+
+									// Logger.debug("onTransaction inStock: " + inStock);
+									Logger.debug("Setting shop #" + shops.get(i).id + "'s stock to " + inStock);
+									try {
+										shops.get(i).setInStock(inStock);
+									} catch (SQLException e) {
+										// TODO Auto-generated catch block
+										e.printStackTrace();
+									}
+									
+									
+								}
+							}
+							
+						}});
+
 				} catch (SQLException e) {
 					Logger.warning("Exception caught while lookign up shops in chunk.");
 					e.printStackTrace();
@@ -96,16 +143,16 @@ public class WorldListener implements Listener {
 
 	}
 
-	public boolean shopExists(Shop shop){
+	public Sign shopExists(Shop shop){
 
 		Block block = shop.getWorld().getBlockAt(shop.getLocation());
 		
 		if (block.getType() == block.getType().WALL_SIGN){
 			
-			return true;
+			return (Sign) block.getState();
 		}
 		
-		return false;
+		return null;
 	}
 	
 }
